fb8afd2400e7be804b790738d5281a32e2257b36
only1techgal <jenn.lipholo@gmail.com>
2024-06-20 16:23:53 +0200

    command with pronts out words followed by a new line using the printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammer but the outcome
+ * is a piece of art, followed by a new line 
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+
+
+{
+	printif("with proper grammer, but the outcome is a piece of art,\n");
+	Return (0);
+
+}
+ 
