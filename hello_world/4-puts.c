1664b75b95bc0358b579caf24dbbb87527fd38e4
only1techgal <jenn.lipholo@gmail.com>
2024-06-20 14:56:36 +0200

    command which prints words followed by a new line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
+
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
  * Return: Always 0 (Success)
  */
 
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
+
 }
+
 
