0f9dcbf4594ea4bc9c802c6b5c05714527ed129c
only1techgal <jenn.lipholo@gmail.com>
2024-06-21 11:42:28 +0200

    command which prints out words followed by a new line using the printf function

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * main - A program that prints a line using the printf function
  * Return: 0 (Success)
-=======
- * main - prints "with proper grammer, but the outcome is a piece of art, followed by a new line
- *
- * Return : Always 0 (Success)
->>>>>>> fa9b091ee0c55fba73bdb8f91ef8255f83e3c824
  */
 int main(void)
 {
-	printif("with proper grrammer, but the outcome is a piece of art,\n");
-	Return(0);
+printf( "with proper grammar, but the outcome is a piece of art,\n" );
+return(0);
 }
 
  
