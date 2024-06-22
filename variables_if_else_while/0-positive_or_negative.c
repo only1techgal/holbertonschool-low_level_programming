64f2384f220595490ce29a90e637511283ebbc06
only1techgal <jenn.lipholo@gmail.com>
2024-06-22 16:32:19 +0200

    command which prints whether the number stored in the variable is a positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main- Entry point
- * Return: Always 0 (Success)
- */
-
+ * Return: Always 0 (Success) */
+ /*
+ * betty-style 0-positive_or_negative.c 
+ * betty-doc 0-positive_or_negative.c */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	return (0);
 	if (n > 0)
-		printf ("%d is positive\n", n);
-	else if (n == 0)
-		printf ("%d is zero\n", n);
-	else 
-		printf ("%d is negative\n", n);
-	return (0);
+                printf ("%d is positive\n", n);
+        else if (n == 0)
+                printf ("%d is zero\n", n);
+        else
+                printf ("%d is negative\n", n);
+        return (0);
 
 }
