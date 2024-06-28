d4e7cef60d8e6c7e47be9b3f2fe7f92a91c945bb
only1techgal <jenn.lipholo@gmail.com>
2024-06-28 14:57:21 +0200

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This multipies integers
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+	int function_pointer;
+	function_pointer = mul;
+	printf("The product of two numbers is:%d", (98, 1024));
+	printf("The product of two numbers is:%d", (-402, 4096));
+}
+
+	
+
