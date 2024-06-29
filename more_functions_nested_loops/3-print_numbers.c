e720dd164b8c3f0ba5f3e417bfd63c3b03e44377
only1techgal <jenn.lipholo@gmail.com>
2024-06-29 12:35:16 +0200

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * print_numbers - writes the character c to stdout
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int n;
 
-	for (n = 0; n <= 9; n++)
-	putchar ((n % 10) + '0');
-	putchar ('\n');
+	for (n = 0; n <= 8; n++)
+	{
+		putchar ((n % 10) + '0');
+	    putchar ('\n');
+	}
+
 }
-
-
