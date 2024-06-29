e16de1f0f80cef4218b3774a8a892b16aa92e680
only1techgal <jenn.lipholo@gmail.com>
2024-06-29 12:36:24 +0200

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 /**
  * print_numbers - writes the character c to stdout
+ * @c: The character to print
+ *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
+	int _putchar(char c)
 
 	for (n = 0; n <= 8; n++)
 	{
