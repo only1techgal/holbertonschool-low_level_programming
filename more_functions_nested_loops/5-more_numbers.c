c0bca8f7fca2aab59c0baa0023eb48d85b7f7180
only1techgal <jenn.lipholo@gmail.com>
2024-06-29 14:43:48 +0200

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * more_numbers - writes the character c to stdout
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+int i, j;
+
+for (int 1 = 0; i < 15; j++)
+{
+	for (j = 0; j <= 14; j++)
+	{
+		if (j >= 10)
+		_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+	}
+	_putchar ("\n");
+}
+
+
+
