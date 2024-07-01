3dd71d6f62e8e940397c293d6946b920936e59cf
only1techgal <jenn.lipholo@gmail.com>
2024-07-01 10:05:09 +0200

    4-print_most_numbers.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,13 +2,5 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-<<<<<<< HEAD
 void print_most_numbers(void);
 
-
-
-=======
-int _putchar(char c);
-void more_numbers(void);
-int _putchar(char c);
->>>>>>> 4a5f7036d4eb7c73beb2483664276a82954b486f
