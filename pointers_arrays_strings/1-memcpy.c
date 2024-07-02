e80dc8d4d4f05cdd4fa9497d4ca20494c8d9aed1
only1techgal <jenn.lipholo@gmail.com>
2024-07-02 17:21:29 +0200

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * @dest: 
+ * @src:
+ * @n
+ */
+void *memcpy(void dest[restrict .n], const void src[restrict .n], size_t n);
+{
+
+}
