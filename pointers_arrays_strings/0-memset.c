7b93b07779e4af1002048505b34353f0251b621f
only1techgal <jenn.lipholo@gmail.com>
2024-07-02 14:59:17 +0200

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stddef.h
+
+/**
+* @s: memory area to be filled
+* @c: char to copy
+* @n: number of times to copy c
+* Return: pointer to the memory area s
+*/
+void *_memset(void *s, int c, size_t n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = (char)c;
+		ptr++;
+		n--;
+	}
+
+	return (s);
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project dives into "C - Even more pointers, arrays and strings"
