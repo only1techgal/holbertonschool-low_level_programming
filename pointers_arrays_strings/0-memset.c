5497c7f9c3f7996780708209979ce5003227d203
only1techgal <jenn.lipholo@gmail.com>
2024-07-04 15:53:54 +0200

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,23 +1,34 @@
-#include <stdio.h>
+#include <string.h>
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 
 /**
-* @s: memory area to be filled
-* @c: char to copy
-* @n: number of times to copy c
-* Return: pointer to the memory area s
-*/
-void *_memset(void *s, int c, size_t n)
+ *_memset - Fills the first n bytes of the memory area pointes to by s,
+with the constant byte c
+ *@buffer: the address of memory to print
+ *size: the size of the memory to print
+ *
+ *@s: Pointer to the memory area to be filled.
+ *@b: Value to set in each byte of the memory area.
+ *@n: Number of bytes to set to the value c
+ *
+ *Return: Always 0.
+ */
+	char *memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
-
-	while (n > 0)
+	for (unsigned int i = 0, i < n; i++)
 	{
-		*ptr = (char)c;
-		ptr++;
-		n--;
+		s[i] = b;
 	}
+return (s);
+}
+	int main(void)
+	{
+	char buffer[98] = {0x00}
 
-	return (s);
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------")
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
