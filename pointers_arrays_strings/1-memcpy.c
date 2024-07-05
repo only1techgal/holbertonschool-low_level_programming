2fd265fc8d0a2b23f336c03747f3d26b9ba1450c
only1techgal <jenn.lipholo@gmail.com>
2024-07-05 15:36:46 +0200

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,16 +3,20 @@
 #include <stddef.h>
 
 /**
- **_memcpy - copies the n bytes from memory
+ *_memcpy - copies the @n bytes from memory area @src
+ to memory area @dest
+ *
  *@dest: Pointer to start copying memory
  *@src: Constant byte to copy memory
  *@n: Number of memory to copy
+ *
+ *Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 unsigned int i = 0
 
-while (i < n);
+while (i < n)
 {
 dest[i] = src[i];
 i++;
