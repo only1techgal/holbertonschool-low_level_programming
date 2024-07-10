42d5311d1fb5d0cb552366f2010232e5d1e3abf5
only1techgal <jenn.lipholo@gmail.com>
2024-07-10 16:47:07 +0200

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ ***alloc_grid - returns pointer to a 2 dimensional arrya of integers
+ *@width: width of the grid
+ *@height: height of the grid
+ *
+ *Return: NULL on failure
+ */
+ int **alloc_grid(int width, int height)
+ {
+    
+ }
