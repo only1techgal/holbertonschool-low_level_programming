a0a0afb2b8ee9952bb49980136ccbeb16218f741
only1techgal <jenn.lipholo@gmail.com>
2024-07-16 12:04:43 +0200

    2-print_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -31,5 +31,8 @@ dog_t *new_dog(char *name, float age, char *owner);
   */
 void print_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
