0cc34a4352401c182c816ded81ad0c42a0a2afbe
only1techgal <jenn.lipholo@gmail.com>
2024-07-18 17:05:11 +0200

    3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ *op_add: Returns the sum
+ *@a: The first integer of the sum which is to be returned
+ *@b: The second intager of the sum which is to be returned
+ *
+ *Return: 1 (success)
+ */
+ int op_add(int a, int b)
+ {
+    return (a + b);
+ }
