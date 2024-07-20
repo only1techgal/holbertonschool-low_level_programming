f8a210b256896b2d2450d4daf8788ae876d948f3
only1techgal <jenn.lipholo@gmail.com>
2024-07-20 10:16:10 +0200

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,28 +3,27 @@
 
 /**
  *get_op_func - Selects the right function to perform the operation by user.
- *@s - The oporator passed to the program.
+ *@s - The operator passed to the program.
  *
- *Return: Pointer to the function which responds to oporator given
+ *Return: Pointer to the function which corresponds to operator given.
  */
- int (*get_op_func(char *s))(int, int)
- {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-    while (i < 5)
-    {
-        if (*s == *(ops[i].op) && s[1] == '\0')
-        return (ops[i].f);
-    i++;
-    
-    }
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (i < 5)
+{
+if (*s == *(ops[i].op) && s[1] == '\0')
+return (ops[i].f);
+i++;
+}
 
-    return (NULL);
- }
+return (NULL);
+}
