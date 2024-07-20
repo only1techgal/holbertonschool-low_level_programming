9faa0ea89acaa64a39c3494f890fc0bfca3bf455
only1techgal <jenn.lipholo@gmail.com>
2024-07-20 09:40:52 +0200

    3-op_functions.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,29 +3,30 @@
 #include "3-calc.h"
 
 /**
- *main - Entry point of the program
- *@arguc: Number of arguments turned into integers.
- *@argv: Array of argument strings.
+ * main - Entry point of the program
+ * @argc: Number of arguments.
+ * @argv: Array of argument strings.
  *
- *Return: 0 on success, or 98, 99, 100, or 101 on certain errors
+ * Return: 0 on success, or 98, 99, or 100 on certain errors
  */
 int main(int argc, char *argv[])
 {
-int (*oprt)(int, int);
+    int (*oprt)(int, int);
 
-if (argv != 4)
-{
-printf("Error\n");
-exit(98);
-}
-oprt = get_op_func(argv[2]);
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-if (!oprt)
-{
-printf("Error\n");
-exit(99);
-}
+    oprt = get_op_func(argv[2]);
 
-printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-return (0);
+    if (!oprt)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+    return (0);
 }
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "calc.h"
 
 /**
  *op_add: Returns the sum
@@ -47,11 +46,7 @@
    */
    int op_div(int a, int b)
    {
-      if (b == 0)
-      {
-         printf("Error\n");
-         exit(100);
-      }
+      return (a / b);
    }
 
    /**
@@ -62,10 +57,7 @@
     *Return: 
     */
     int op_mod(int a, int b)
-    {
-      if (b == 0)
-      {
-         printf("Error\n");
-         exit(100);
-      }
-    }
+   {
+      return (a % b);
+   }
+    
