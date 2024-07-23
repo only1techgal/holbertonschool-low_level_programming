ace5e9088aa72bfbbdb45d18dd1e682fb2e1d7a2
only1techgal <jenn.lipholo@gmail.com>
2024-07-23 13:28:51 +0200

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,7 +5,7 @@
 /**
  * print_line - draws a straight line in the terminal
  * @n: The number of times the character '_' should be printed
- * 
+ *
  * If n is 0 or less, only prints a newline.
  */
 void print_line(int n)
@@ -16,7 +16,6 @@ if (n <= 0)
 putchar('\n');
 return;
 }
-    
 for (i = 0; i < n; i++)
 {
 putchar('_');
