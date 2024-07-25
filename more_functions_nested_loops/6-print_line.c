3b403e8e6b354b7b737bc971a1cf59b8272ae280
only1techgal <jenn.lipholo@gmail.com>
2024-07-25 10:32:52 +0200

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,12 @@ void print_line(int n)
 int i;
 if (n <= 0)
 {
-putchar('\n');
+_putchar('\n');
 return;
 }
 for (i = 0; i < n; i++)
 {
-putchar('_');
+_putchar('_');
 }
-putchar('\n');
+_putchar('\n');
 }
