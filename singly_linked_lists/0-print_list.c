3fce55dfa10ed8a2198c11d3e71cdbc4d1f494e1
only1techgal <jenn.lipholo@gmail.com>
2024-07-30 15:36:58 +0200

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ *print_list - Prints all elements of a header file list
+ *@h: The pinter to the head
+ *
+ *Description: Printed list node structure
+ *
+ *Return: 0 (nil)if string is NULL
+ */
+size_t print_list(const list_t *h)
+{
+size_t node_count = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+node_count++;
+}
+return (node_count);
+}
