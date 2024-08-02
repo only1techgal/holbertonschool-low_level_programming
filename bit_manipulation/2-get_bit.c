a30b12f5f1c24f9ea0d02163704e94a3396d562f
only1techgal <jenn.lipholo@gmail.com>
2024-08-02 11:22:41 +0200

    3-set_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,7 +4,6 @@
 /**
  *get_bit - Returns the value of a bit at a given index
  *@n: The integer
- *
  *@index: The index of the bit to get, starting from 0
  *Return: VAluse at index or -1 if there is an error
  */
