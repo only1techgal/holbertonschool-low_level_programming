6dd860c5154e393191804b45f593efb75b5a5bad
only1techgal <jenn.lipholo@gmail.com>
2024-08-03 10:20:23 +0200

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -25,31 +25,30 @@ char *buffer;
 if (filename == NULL)
 return (0);
 
-buffer = malloc(letters);
-if (buffer == NULL)
-return (0);
-
 fd = open(filename, O_RDONLY);
 if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
 {
-perror("Error opening file");
-free(buffer);
+close(fd);
 return (0);
 }
 
 bytes_read = read(fd, buffer, letters);
 if (bytes_read == -1)
 {
-perror("Error reading file");
 free(buffer);
 close(fd);
 return (0);
 }
+
+buffer[bytes_read] = '\0';
 
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
-perror("Error writing to stdout");
 free(buffer);
 close(fd);
 return (0);
@@ -57,19 +56,5 @@ return (0);
 
 free(buffer);
 close(fd);
-
-{
-char num_buf[32];
-int num_len;
-
-num_len = snprintf(num_buf, sizeof(num_buf), "%d", (int)bytes_written);
-if (num_len >= 0 && num_len < sizeof(num_buf))
-{
-write(STDOUT_FILENO, "\n(printed chars: ", 17);
-write(STDOUT_FILENO, num_buf, num_len);
-write(STDOUT_FILENO, ")\n", 2);
-}
-}
-
-return (bytes_read);
+return (bytes_written);
 }
